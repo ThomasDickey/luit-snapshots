71ad8c8b65129e8ac3176fff237c33667ef3062f
Thomas E. Dickey <dickey@invisible-island.net>
2010-06-05 12:32:04 -0400

    snapshot of project "luit", label t20100605

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 /*
- * $XTermId: version.h,v 1.3 2010/06/01 00:52:19 tom Exp $
+ * $XTermId: version.h,v 1.4 2010/06/05 12:21:26 tom Exp $
  *
  * http://invisible-island.net/luit/
  */
-#define LUIT_VERSION "1.0-20100601"
+#define LUIT_VERSION "1.0-20100605"
