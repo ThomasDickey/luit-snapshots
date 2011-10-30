6a25cfadc4a0f80256c63f9613482719c40170cd
Thomas E. Dickey <dickey@invisible-island.net>
2011-10-30 14:41:39 -0400

    snapshot of project "luit", label t20111030

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 /*
- * $XTermId: version.h,v 1.13 2011/10/28 22:32:49 tom Exp $
+ * $XTermId: version.h,v 1.14 2011/10/30 14:41:39 tom Exp $
  *
  * http://invisible-island.net/luit/
  */
-#define LUIT_VERSION "1.2-20111028"
+#define LUIT_VERSION "1.2-20111030"
