1080c6c0a38f3948f35f6cdf6b7e32baa6a6bfb7
Thomas E. Dickey <dickey@invisible-island.net>
2022-01-12 00:55:28 +0000

    snapshot of project "luit", label t20220111

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 /*
- * $XTermId: version.h,v 1.61 2021/02/18 14:51:57 tom Exp $
+ * $XTermId: version.h,v 1.62 2022/01/11 22:58:31 tom Exp $
  *
  * http://invisible-island.net/luit/
  */
-#define LUIT_VERSION "2.0-20210218"
+#define LUIT_VERSION "2.0-20220111"
